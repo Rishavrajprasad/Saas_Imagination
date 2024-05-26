27ae9a307c7704592aefdf606bd4761e52add02a
Rishavrajprasad <rishavrajprasad800@gmail.com>
2024-05-26 21:00:50 +0530

    Added navbar and sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import MobileNav from "@/components/shared/MobileNav";
+import Sidebar from "@/components/shared/Sidebar";
+import React from "react";
 
-const Layout = ({children}: {children: React.ReactNode}) => {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className='root'>
-        <div className="root-container">
-            <div className="wrapper">
-                {children}
-            </div>
-        </div>
-        </main>
-  )
-}
+    <main className="root">
+      <Sidebar />
+      <MobileNav />
+      <div className="root-container">
+        <div className="wrapper">{children}</div>
+      </div>
+    </main>
+  );
+};
 
-export default Layout
+export default Layout;
