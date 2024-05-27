c0ffc7be3f0b1f8843a616164e345080b5ab560b
Ankita96-pi <btech60309.21@bitmesra.ac.in>
2024-05-27 21:24:45 +0530

    created transformation page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,19 +4,6 @@ import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-<<<<<<< HEAD
-    <main className='root'>
-      {/* Sidebar */}
-      {/* MobileNav */}
-        <div className="root-container">
-            <div className="wrapper">
-                {children}
-            </div>
-        </div>
-        </main>
-  )
-}
-=======
     <main className="root">
       <Sidebar />
       <MobileNav />
@@ -26,6 +13,5 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
     </main>
   );
 };
->>>>>>> fca5274a9f91f7750ca8e7719750bef972d145d8
 
 export default Layout;
